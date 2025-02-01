b8e5d04f17bad7bd11ae34272e094b3ea794a942
TallesAndrade <tallesandrade7@gmail.com>
2025-02-01 01:18:32 -0300

    feat: aula arrays

---
+++ src/com/talles/maratonajava/introducao/aula07/Aula07Arrays02.java
@@ -0,0 +1,19 @@
+package com.talles.maratonajava.introducao.aula07;
+
+public class Aula07Arrays02 {
+
+    public static void main(String[] args) {
+        //byte, short, int, double, float = 0
+        //char = '\0000' ' '
+        // boolean = false
+        //String = null
+
+        int[] idades = new int[3];
+        idades[0] = 17;
+        idades[1] = 15;
+        idades[2] = 38;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
